3a294b0d3ab5a6d5517d16e787a83a8cbaa1706c
Astora <mefsaal@gmail.com>
2023-09-08 19:02:05 -0300

    Depecrate Anbox

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -1,38 +1,23 @@
 #!/usr/bin/env bash
 
 # First use dkms to remove the modules:
-sudo dkms remove anbox-ashmem/1
-sudo dkms remove anbox-binder/1
+sudo dkms remove waydroid-binder/1
 
 # Then remove the module sources from /usr/src/:
-sudo rm -rf /usr/src/anbox-ashmem-1
-sudo rm -rf /usr/src/anbox-binder-1
+sudo rm -rf /usr/src/waydroid-binder-1
 
 # Finally remove the configuration files:
-sudo rm -f /etc/modules-load.d/anbox.conf
-sudo rm -f /lib/udev/rules.d/99-anbox.rules
+sudo rm -f /etc/modules-load.d/waydroid.conf
+sudo rm -f /lib/udev/rules.d/99-waydroid.rules
 
 # Verify remove by trying to load the modules and checking the created devices:
-failed_checks=0
-if sudo modprobe ashmem_linux > /dev/null 2>&1; then
-    failed_checks=1
-else
-    failed_checks=0
-fi
-
 if sudo modprobe binder_linux > /dev/null 2>&1; then
     failed_checks=1
 else
     failed_checks=0
 fi
 
-if lsmod | grep -e ashmem_linux -e binder_linux > /dev/null 2>&1; then
-    failed_checks=1
-else
-    failed_checks=0
-fi
-
-if ls -alh /dev/binder /dev/ashmem > /dev/null 2>&1; then
+if lsmod | grep -e binder_linux > /dev/null 2>&1; then
     failed_checks=1
 else
     failed_checks=0
--- scripts/build-with-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker pull ubuntu:16.04
-docker run -i -t -v $PWD:/anbox ubuntu:16.04 /anbox/scripts/clean-build.sh
